b6b9f201061cc1cc3a21043ef4c5824abd8d0a3f
ControlNet <smczx@hotmail.com>
2026-01-13 11:55:59 +0000

    Update AI Terminal model resource locations and add new JSON files for on/off states. Change model paths to use 'chatae' namespace and create new models for ai_terminal_on and ai_terminal_off with appropriate textures.

--- common-1.20.1/src/main/java/space/controlnet/chatae/common/part/AiTerminalPartModelIds.java
+++ common-1.20.1/src/main/java/space/controlnet/chatae/common/part/AiTerminalPartModelIds.java
@@ -3,8 +3,8 @@ package space.controlnet.chatae.common.part;
 import net.minecraft.resources.ResourceLocation;
 
 public final class AiTerminalPartModelIds {
-    public static final ResourceLocation MODEL_OFF = new ResourceLocation("ae2", "part/terminal_off");
-    public static final ResourceLocation MODEL_ON = new ResourceLocation("ae2", "part/terminal_on");
+    public static final ResourceLocation MODEL_OFF = new ResourceLocation("chatae", "part/ai_terminal_off");
+    public static final ResourceLocation MODEL_ON = new ResourceLocation("chatae", "part/ai_terminal_on");
 
     private AiTerminalPartModelIds() {
     }
